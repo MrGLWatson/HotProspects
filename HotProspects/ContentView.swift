72031114962294ffd46024a36edf48cee7d2b98e
Gary Watson <Gary_Watson@Blueyonder.co.uk>
2020-07-30 11:07:13 +0100

    Manually publishing ObservableObject changes

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -5,55 +5,31 @@
 //  Created by Gary Watson on 28/07/2020.
 //  Copyright © 2020 Gary Watson. All rights reserved.
 //
-// Understanding Swift's Result Type
+// Manually publishing ObservableObject changes
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+            // so we could add other code here to do stuff
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
 
 struct ContentView: View {
+    @ObservedObject var updater = DelayedUpdater()
     var body: some View {
-        Text("Hello")
-            .onAppear {
-                self.fetchData(from: "https://apple.com") {
-                    result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network Problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-          }
-    }
-    
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void)  {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unknown))
-                }
-            }
-        }.resume()
+            Text("Value is: \(updater.value)")
     }
 }
 
